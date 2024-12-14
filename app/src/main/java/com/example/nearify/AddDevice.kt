64b2ad70f5a467c25a2c1511c1f98faa8f3bf7b1
Elaraby218 <152742814+Elaraby218@users.noreply.github.com>
2024-12-15 01:09:01 +0200

    THis funking is done

--- app/src/main/java/com/example/nearify/AddDevice.kt
+++ app/src/main/java/com/example/nearify/AddDevice.kt
@@ -1,36 +1,20 @@
 package com.example.nearify
 
-import android.content.Context
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
-class addDevices : AppCompatActivity() {
+class AddDevice : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_add_devices)
-        GlobalScope.launch(Dispatchers.IO) {
-            val db = MainActivity.db
-            val allAction = db.actionDao().getAllActions
-            withContext(Dispatchers.Main) {
-                val context: Context = this@addDevices;
-            }
-
-
-        }
+        setContentView(R.layout.activity_add_device)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        // get bluetooth devices
-
     }
 }
